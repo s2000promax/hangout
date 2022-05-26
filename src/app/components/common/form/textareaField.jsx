1539d6018da6d79810773121878c646eb94384bd
Stan <s2000ProMax@gmail.com>
2022-05-27 00:52:17 +0300

    feat: add users comments

--- src/app/components/common/form/textareaField.jsx
+++ src/app/components/common/form/textareaField.jsx
@@ -1,55 +1,39 @@
-import React, { useState } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 
-const TextField = ({ label, type, name, placeholder, value, onChange, error }) => {
-  const [showPassword, setShowPassword] = useState(false);
+const TextareaField = ({ label, name, placeholder, value, rows, onChange, error }) => {
   const handleChange = ({ target }) => {
     onChange({ name: target.name, value: target.value });
   };
   const getInputClasses = () => {
     return 'form-control' + (error ? ' is-invalid' : '');
   };
-  const toggleShowPassword = () => {
-    setShowPassword(prevState => !prevState);
-  };
   return (
     <div className='mb-4'>
       <label htmlFor={name}>{label}</label>
       <div className='input-group has-validation'>
-        <input
+        <textarea
           className={getInputClasses()}
-          type={showPassword ? 'text' : type}
           name={name}
           placeholder={placeholder}
           id={name}
           value={value}
+          rows={rows}
           onChange={handleChange}
         />
-        {
-          type === 'password' && (
-            <button
-              className='btn btn-outline-secondary'
-              type='button'
-              onClick={toggleShowPassword}
-            >
-              <i className={'bi bi-eye' + (showPassword ? '-slash' : '')}></i>
-            </button>
-          )
-        }
         {error && <div className='invalid-feedback'>{error}</div>}
       </div>
     </div>
   );
 };
-
-TextField.propTypes = {
+TextareaField.propTypes = {
   label: PropTypes.string,
-  type: PropTypes.string,
   name: PropTypes.string,
   placeholder: PropTypes.string,
   value: PropTypes.string,
+  rows: PropTypes.string,
   onChange: PropTypes.func,
   error: PropTypes.string
 };
 
-export default TextField;
+export default TextareaField;
--- src/app/utils/utils.js
+++ src/app/utils/utils.js
@@ -62,3 +62,38 @@ export const createHeaders = (headers, callBackFunc) => {
         : null
     );
 };
+
+export const formatDateLabel = (timeStamp) => {
+  const msScale = 1000;
+  const minScale = 60 * msScale;
+  const hourScale = 60 * minScale;
+  const dayScale = 24 * hourScale;
+  const monthScale = 30 * dayScale;
+  const yearScale = 12 * monthScale;
+
+  const subTimeStamp = Date.now() - timeStamp;
+  const year = Math.floor(subTimeStamp / yearScale);
+
+  const diffForMonth = subTimeStamp - (year * yearScale);
+  const month = Math.floor(diffForMonth / monthScale);
+
+  const diffForDays = diffForMonth - (month * monthScale);
+  const days = Math.floor(diffForDays / dayScale);
+
+  const diffForHours = diffForDays - (days * dayScale);
+  const hours = Math.floor(diffForHours / hourScale);
+
+  const diffForMinutes = diffForHours - (hours * hourScale);
+  const minutes = Math.floor(diffForMinutes / minScale);
+
+  const getStringFormat = (period, describe) => !!period ? `${period} ${describe} ` : '';
+
+  const result = getStringFormat(year, 'year')
+    + getStringFormat(year, 'year')
+    + getStringFormat(month, 'month')
+    + getStringFormat(days, 'days')
+    + getStringFormat(hours, 'hours')
+    + (getStringFormat(minutes, 'minutes') ? getStringFormat(minutes, 'minutes') + 'ago' : 'right now');
+
+  return result;
+};
