4c5557291a8ae3634abb93bf45ec0fe90004e401
Stan <s2000ProMax@gmail.com>
2022-06-04 23:17:55 +0300

    feat: firebase

--- src/app/services/http.service.js
+++ src/app/services/http.service.js
@@ -1,18 +1,46 @@
 import axios from 'axios';
 import { toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-import config from '../config.json';
+import configFile from '../config.json';
 
-axios.defaults.baseURL = config.apiEndPoint;
+axios.defaults.baseURL = configFile.apiEndPoint;
 
-axios.interceptors.response.use(res => res, function (error) {
-  const expectedErrors = error.response && error.response.status >= 400 && error.response.status < 500;
-  if (!expectedErrors) {
-    console.log('Error:', error);
-    toast.error('Something was wrong');
+axios.interceptors.request.use(
+  function (config) {
+    if (configFile.isFirebase) {
+      const containSlash = /\/$/gi.test(config.url);
+      config.url = (containSlash ? config.url.slice(0, -1) : config.url) + '.json';
+      console.log(config.url);
+    }
+    return config;
+  }, function (error) {
+    return Promise.reject(error);
   }
-  return Promise.reject(error);
-});
+);
+
+function transformData(data) {
+  return data
+    ? Object.keys(data).map(key => ({
+      ...data[key]
+    }))
+    : [];
+}
+
+axios.interceptors.response.use(
+  (res) => {
+    if (configFile.isFirebase) {
+      res.data = { content: transformData(res.data) };
+    }
+    console.log(res.data);
+    return res;
+  }, function (error) {
+    const expectedErrors = error.response && error.response.status >= 400 && error.response.status < 500;
+    if (!expectedErrors) {
+      console.log('Error:', error);
+      toast.error('Something was wrong');
+    }
+    return Promise.reject(error);
+  });
 
 const httpService = {
   get: axios.get,
