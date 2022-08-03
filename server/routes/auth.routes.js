cb97fa9cc5fa4064150e7ad9b4e6e2dfed44ec1f
Stan <s2000ProMax@gmail.com>
2022-08-03 18:15:59 +0400

    fix: add gitignore

--- server/routes/auth.routes.js
+++ server/routes/auth.routes.js
@@ -1,8 +1,49 @@
 const express = require('express');
+const bcrypt = require('bcryptjs');
+const User = require('../models/User');
+
+const { generateUserData } = require('../utils/helper');
+const tokenService = require('../services/token.service');
+
 const router = express.Router({ mergeParams: true });
 
+// /api/auth/signUp
+// 1. get data from req (email, password)
+// 2. check if users already exists
+// 3.hash password
+// 4. creat user
+// 5. generate tokens
+//  npm install bcryptjs jsonwebtoken express-validator
 router.post('/signUp', async (req, res) => {
+  try {
+    const { email, password } = req.body;
 
+    const existingUser = await User.findOne({ email });
+    if (existingUser) {
+      return res.status(400).json({
+        error: {
+          message: 'EMAIL_EXISTS',
+          code: 400
+        }
+      })
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 12);
+    const newUser = await User.create({
+      ...generateUserData(),
+      ...req.body,
+      password: hashedPassword
+    });
+
+    const tokens = tokenService.generate({ _id: newUser._id });
+    res.status(201).send({ ...tokens, userId: newUser._id })
+
+
+  } catch (error) {
+    res.status(500).json({
+      message: 'Server error. Please try again later'
+    })
+  }
 });
 
 router.post('/signInWithPassword', async (req, res) => {
---
+++ server/services/token.service.js
@@ -0,0 +1,11 @@
+class TokenService {
+  // return:
+  // accessToken
+  // refreshToken
+  // expiresIn
+  generate() {
+
+  }
+}
+
+module.exports = new TokenService();
